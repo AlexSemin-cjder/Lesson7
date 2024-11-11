7ebe623424ba2b9a496648f5a03b134db9e1783d
AlexSemin-cjder <119964665+AlexSemin-cjder@users.noreply.github.com>
2024-11-11 12:36:50 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var_ =  3, 2, 1, 'c', 'b', 'a'
+print(immutable_var_)
+print(immutable_var_[2])
+ # immutable_var_[2] = 5  кортежи - это объекты неизменяемого типа в python
+print(immutable_var_) 
+mutable_list_ = ['c', 'b', 'a', 3, 2, 1]
+print(mutable_list_)
+print(mutable_list_ [1])
+mutable_list_[1] = 'p'
+print(mutable_list_ )
